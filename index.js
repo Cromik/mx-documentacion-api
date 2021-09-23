68f89216fa0fc6347b7bab8f730f53e807c11f5d
MichaelMelo <michaelmario.mp@gmail.com>
2021-09-22 23:00:33 -0500

    documentacion get para productos, ivas, tipos de identificacion

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./apis/mx-facturacion.json');
+const swaggerDocument = require('./apis/mx-general.json');
 
-app.use('/mx-facturacion', swaggerUi.serve, swaggerUi.setup(swaggerDocument, null));
+app.use('/mx-general', swaggerUi.serve, swaggerUi.setup(swaggerDocument, null));
 
-app.listen(3005, () => console.log("localhost:3005/mx-facturacion"));
+app.listen(3005, () => console.log("http://localhost:3005/mx-general"));
