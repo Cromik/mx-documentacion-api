0c3616bc86a9a8cbea0d166fe45a4358b52a047e
MichaelMelo <michaelmario.mp@gmail.com>
2021-09-30 21:23:20 -0500

    modulo de seguridad init

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./apis/mx-general.json');
+const swaggerGeneral = require('./apis/mx-general.json');
+const swaggerSeguridad = require('./apis/mx-seguridad.json');
 
-app.use('/mx-general', swaggerUi.serve, swaggerUi.setup(swaggerDocument, null));
+app.use('/mx-general', swaggerUi.serve, swaggerUi.setup(swaggerGeneral, null));
+app.use('/mx-seguridad', swaggerUi.serve, swaggerUi.setup(swaggerSeguridad, null));
 const port = process.env.PORT || 3005;
 app.listen(port, () => console.log("http://localhost:"+port+"/mx-general"));
